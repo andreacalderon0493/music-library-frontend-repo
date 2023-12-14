839902b7594bd9fea4479b80fe9d4a88d536f7de
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2023-12-14 16:45:25 -0500

    added song data from API by cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import MusicTable from "./components/MusicTable/MusicTable";
+import axios from "axios";
 
 function App() {
+  const fetchSongs = async () => {
+    const response = await axios.get("https://localhost:7257/api/Songs");
+    console.log(response);
+  };
   return (
     <div className="App">
       <Header />
+      <button onClick={fetchSongs}>Fetch Songs</button>
       <MusicTable />
     </div>
   );
